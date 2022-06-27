a1812ca2e2bb95452d6f1e91f24fdc1f24de95d1
mariesophiecd <mariesophie.cduran@gmail.com>
2022-06-27 11:59:19 +0100

    created DB schemas and seeding, completed init.js and seedDev.js

--- db/migrations/2_create_habits.sql
+++ db/migrations/2_create_habits.sql
@@ -1,4 +1,10 @@
---
--- ─── DATABASE: HABITS ───────────────────────────────────────────────────────────
---
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    name varchar(200),
+    frequency int,
+    time timestamp,
+    user_id int
+);
 
