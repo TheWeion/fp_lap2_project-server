2839d5938c9418e64a8198008e519696190d8182
mariesophiecd <mariesophie.cduran@gmail.com>
2022-06-27 12:40:09 +0100

    created Routes files and added changes to README

---
+++ api/routes/habits.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const habitsController = require('../controllers/habits')
+
+router.get('/', habitsController.index);
+router.get('/:id', habitsController.show);
+router.post('/', habitsController.create);
+router.delete('/:id', habitsController.destroy);
