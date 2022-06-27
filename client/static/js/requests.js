0bbb473695ea70cfa69073c8bd111b4beb2540fa
BenTidball <bentidball97@gmail.com>
2022-06-27 14:52:26 +0100

    register request

--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -3,7 +3,7 @@
 //
 
 //Requires values of 
-function registerFormValidation(username, email, password, passwordConfirm){
+async function registerFormValidation(username, email, password, passwordConfirm){
     if(username && email){
       if(password == passwordConfirm){
         return true;
@@ -12,3 +12,36 @@ function registerFormValidation(username, email, password, passwordConfirm){
         return new Error("Invalid or incorrect information")
     }
 }
+
+const url = "localhost:3000";
+// TEST PAYLOAD //
+const testPayload = {username: "user", password:"pass", email: "user@gamil.com"};
+
+//Post request for submitting user data
+async function submitRegister(){
+  try {
+    const config = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(`${url}`, config)
+    //const json = await response.json()
+    if (response.ok) {
+      console.log(response);
+        return response
+    } 
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+//Get request for obtaining user data
+function submitLogin(){
+  
+}
+
+submitRegister(testPayload);
