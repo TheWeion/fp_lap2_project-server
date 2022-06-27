9260c6dcdfcc37b6a50d660b42b02e44bb5ba742
“TheWeion” <ianterryfallows@gmail.com>
2022-06-27 16:38:48 +0100

    Implemented Cryptography

--- api/index.js
+++ api/index.js
@@ -7,8 +7,10 @@
 //
 
 const app = require('./server');
+const dotenv = require('dotenv');
 const port = process.env.PORT || 3000;
 
 // ────────────────────────────────────────────────────────────────────────────────
 
+dotenv.config();
 app.listen(port, () => console.log(`Server listening on port ${port}`));
--- db/migrations/1_create_users.sql
+++ db/migrations/1_create_users.sql
@@ -4,7 +4,6 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     username varchar(200) NOT NULL UNIQUE,
     email varchar(100) NOT NULL UNIQUE,
-    password_digest varchar(500) NOT NULL,
-    habits_id INTEGER,
+    password varchar(500) NOT NULL,
     created_at timestamp NOT NULL DEFAULT now()
 );
